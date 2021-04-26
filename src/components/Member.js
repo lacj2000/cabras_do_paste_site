26f49be2ffd2c0aeccb06908a56d6ba387363246
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2021-04-25 21:06:55 -0300

    update - add github

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
+import { AiFillGithub } from "react-icons/ai";
+
 // import { Col, Row } from 'react-bootstrap';
-import Styles from '../styles/components/Member.module.css';
+import Styles from '../styles/components/Member.module.css';  
 
 function Member({member}){
-  const { member_image, name, email } = member;
+  const { member_image, name, email, github } = member;
   return (
     <div className={Styles.memberComponent}>
       <Image src={`/${member_image}`} width='200px' height='200px' roundedCircle/>
       <div>
         <h1>{name}</h1>
         <p>{email}</p>
+        <a href={`https://github.com/${github}`}><AiFillGithub size='1.5em' />{github}</a>
       </div>
     </div>
   );
